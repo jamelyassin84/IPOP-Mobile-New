f3436e75fdcfb1a158c512e22773c4eb4b870be3
Jamel Yassin <jamelyassin84@gmail.com>
2021-08-13 18:29:57 +0800

    program-area

--- modules/programs/ProgramArea.tsx
+++ modules/programs/ProgramArea.tsx
@@ -1,21 +1,79 @@
+import { AntDesign, MaterialCommunityIcons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 import React, { FC } from 'react';
-import { Text } from 'react-native';
-import { View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import PageTitle from '../../components/PageTitle';
 import WithRefreshComponent from '../../components/WithRefreshComponent';
+import Colors from '../../constants/Colors';
 import Container from '../../constants/Layout';
+import { BaseService } from '../../environments/base.service';
+import { ProgramAreas_API } from '../../environments/Enums';
+import useColorScheme from '../../hooks/useColorScheme';
+import style from '../../styles/app/about/about.index.style'
 
 type Props = {};
 
 const ProgramAreaScreen: FC<Props> = ( props ) => {
+
+    const navigation = useNavigation();
+    const colorScheme = useColorScheme();
+
+    const [ programs, setPrograms ]: any = React.useState( [] )
+    const [ isLoading, setLoading ] = React.useState( false )
+
+    React.useEffect( () => {
+        getProgramArea()
+    }, [] )
+
+    const onRefresh = () => {
+        getProgramArea();
+    };
+
+    const getProgramArea = () => {
+        setLoading( true )
+        new BaseService( ProgramAreas_API.ProgramAreas ).fetch( '' ).then( ( data: any[] ) => {
+            setPrograms( data )
+            setLoading( false )
+        } )
+    }
+
     return (
         <Container>
             <PageTitle title="Program Areas" />
-            <WithRefreshComponent loading={false} onRefresh={() => { void 0 }}>
+            <WithRefreshComponent loading={isLoading} onRefresh={() => onRefresh}>
+                {
+                    programs.map( ( program: ProgramArea, index: number ) => (
+                        <TouchableOpacity key={index} style={style.container}
+                            onPress={() => navigation.navigate( program.id )}>
+                            <MaterialCommunityIcons name="radar" size={35} color='#0D47A1' />
+                            <View style={{ paddingRight: 10, flex: 1 }}>
+                                <Text style={[ style.title, { color: Colors[ colorScheme ].text } ]}>{program.title}</Text>
+                                <Text style={{ color: 'gray', fontSize: 12, marginLeft: 15, lineHeight: 20 }}>{program.description}</Text>
+                            </View>
+                            <AntDesign name="right" size={24} color="#ccc" />
+                        </TouchableOpacity>
+                    ) )
 
+                }
             </WithRefreshComponent>
         </Container>
     );
 };
+
+interface ProgramArea {
+    id: string
+    title: string
+    description: string
+    activities: Activities
+}
+
+interface Activities {
+    files: any
+    program_area_id: number
+    title: number
+    description: string
+    created_at: any
+}
+
 
 export default ProgramAreaScreen;
