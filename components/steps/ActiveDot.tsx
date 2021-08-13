79234b22f08392359900c9b873d7591b82871a06
Jamel Yassin <jamelyassin84@gmail.com>
2021-08-13 21:29:30 +0800

    fixes

--- components/steps/ActiveDot.tsx
+++ components/steps/ActiveDot.tsx
@@ -5,9 +5,7 @@ import style from '../../styles/steps/step.style'
 type Props = {};
 
 const ActiveDot: FC<Props> = ( props ) => {
-    return (
-        <View style={style.active}></View>
-    );
+    return <View style={style.active}></View>
 };
 
 export default ActiveDot;
--- components/steps/Dot.tsx
+++ components/steps/Dot.tsx
@@ -6,9 +6,7 @@ import style from '../../styles/steps/step.style'
 type Props = {};
 
 const Dot: FC<Props> = ( props ) => {
-    return (
-        <View style={style.dot}></View>
-    );
+    return <View style={style.dot}></View>
 };
 
 export default Dot;
