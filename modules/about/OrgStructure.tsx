d0b22699785f9ffd5667a6ee17b485ca5113bdc0
Jamel Yassin <jamelyassin84@gmail.com>
2021-08-13 12:26:56 +0800

    Org Structure

--- modules/about/OrgStructure.tsx
+++ modules/about/OrgStructure.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from 'react';
-import { Text } from 'react-native';
+import { Text, Image, Dimensions } from 'react-native';
 import CommonHeader from '../../components/headers/CommonHeader';
 import Colors from '../../constants/Colors';
 import Container from '../../constants/Layout';
+import { BaseService } from '../../environments/base.service';
+import { About_API } from '../../environments/Enums';
 import useColorScheme from '../../hooks/useColorScheme';
 
 type Props = {};
@@ -11,9 +13,19 @@ const OrgStructure: FC<Props> = ( props ) => {
 
     const colorScheme = useColorScheme();
 
+    const [ image, setImage ]: any = React.useState( require( '../../assets/logo/ipo-logo.png' ) )
+
+    React.useEffect( () => {
+
+        new BaseService( About_API.OrganizationalChart ).fetch( '' ).then( ( data: any ) => {
+            if ( data.photo !== undefined ) setImage( { uri: data.photo.uri } )
+        } )
+    }, [] )
+
     return (
         <Container>
             <CommonHeader title="Organizational Structure" backgroundColor={Colors[ colorScheme ].background} />
+            <Image source={image} style={{ resizeMode: 'contain', flex: 1 }} />
         </Container>
     );
 };
