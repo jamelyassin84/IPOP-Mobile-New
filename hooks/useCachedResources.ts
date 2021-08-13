3968a1717f3b8883f5abbe47335c878d345c984c
Jamel Yassin <jamelyassin84@gmail.com>
2021-08-13 21:35:06 +0800

    fixes

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -1,31 +1,25 @@
-import { Ionicons } from '@expo/vector-icons';
-import * as Font from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import * as React from 'react';
+import { Ionicons } from '@expo/vector-icons'
+import * as Font from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+import * as React from 'react'
 
 export default function useCachedResources() {
-  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
+	const [isLoadingComplete, setLoadingComplete] = React.useState(false)
 
-  React.useEffect(() => {
-    async function loadResourcesAndDataAsync() {
-        try {
-            SplashScreen.preventAutoHideAsync();
+	React.useEffect(() => {
+		const loadResourcesAndDataAsync = async () => {
+			try {
+				SplashScreen.preventAutoHideAsync()
+				await Font.loadAsync({ ...Ionicons.font, 'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf') })
+			} catch (e) {
+				console.warn(e)
+			} finally {
+				setLoadingComplete(true)
+				SplashScreen.hideAsync()
+			}
+		}
+		loadResourcesAndDataAsync()
+	}, [])
 
-            await Font.loadAsync({
-            ...Ionicons.font,
-            'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
-            });
-        } catch (e) {
-        
-            console.warn(e);
-        } finally {
-            setLoadingComplete(true);
-            SplashScreen.hideAsync();
-        }
-    }
-
-    loadResourcesAndDataAsync();
-  }, []);
-
-  return isLoadingComplete;
+	return isLoadingComplete
 }
