96a2fbd14a2a606426ea326e8a75e59300fc0614
Jamel Yassin <jamelyassin84@gmail.com>
2022-05-18 22:45:42 +0400

    Updated SDK

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
 	production: true,
-	api: 'https://ipop-test.tk/api/',
+	api: 'https://api.ipop-app.com/api/',
+	// api: 'http://192.168.1.103:8000/api/',
 }
--- environments/environtment.ts
+++ environments/environtment.ts
@@ -1,4 +1,5 @@
 export const environment = {
 	production: false,
-	api: 'http://192.168.1.107:8000/api/',
+	// api: 'http://192.168.1.107:8000/api/',
+	api: 'https://api.ipop-app.com/api/',
 }
